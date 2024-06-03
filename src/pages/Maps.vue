821941da4659526adcefd5f49139d35b8e33849f
Stefano Alvarado <70769238+U20170976@users.noreply.github.com>
2024-06-03 18:52:12 -0500

    Tomcat

--- src/pages/Maps.vue
+++ src/pages/Maps.vue
@@ -772,7 +772,7 @@
     },
     methods: {
      fetchAeropuertos() {
-       axios.get('http://localhost:8080/api/aeropuertos')
+       axios.get('https://inf226-982-3c.inf.pucp.edu.pe/tomcat/api/aeropuertos')
          .then(response => {
            this.aeropuertos = response.data;
            this.geojsonAeropuertos.features = this.aeropuertos.map(a => ({
