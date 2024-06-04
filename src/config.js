ff4f834a2142e83ccee73949ce38220da3fb5d1a
EnriqueIgnacio1011 <82422553+EnriqueIgnacio1011@users.noreply.github.com>
2024-06-04 13:51:09 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -8,5 +8,6 @@ export default {
     primaryGradient: ['rgba(76, 211, 150, 0.1)', 'rgba(53, 183, 125, 0)', 'rgba(119,52,169,0)'],
   },
   urlBase: 'http://localhost'
+  //urlBase: 'http://localhost:8080' //con el puerto modificado de Renzo
   //urlBase: 'https://inf226-982-3c.inf.pucp.edu.pe/tomcat'
 }
