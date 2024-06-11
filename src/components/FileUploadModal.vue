803dacf049a742149bac1b926e256fbcb83c0aec
EnriqueIgnacio1011 <82422553+EnriqueIgnacio1011@users.noreply.github.com>
2024-06-11 14:01:42 -0500

    subida de archivos

--- src/components/FileUploadModal.vue
+++ src/components/FileUploadModal.vue
@@ -1,39 +1,184 @@
 <template>
   <div v-if="isVisible" class="popup-overlay" @click="closeModal">
-    <div class="popup" @click.stop>
-      <h3>Subir Archivos</h3>
-      <input type="file" @change="handleFileUpload" ref="fileInput" />
-      <div class="drop-zone" @dragover.prevent @drop="handleDrop">
-        Arrastra y suelta tus archivos aquí
+    <div
+      class="popup"
+      @click.stop
+      @mousedown="startDrag"
+      :style="modalStyle"
+      ref="modal"
+    >
+      <button @click="closeModal" class="close-button">&times;</button>
+      <h1>Subida de Archivo</h1>
+      <div class="file-input-container">
+        <input type="file" @change="handleFileUpload" ref="fileInput" id="file-input" />
+        <label for="file-input" class="file-input-label">Seleccionar Archivo</label>
       </div>
-      <button @click="closeModal" class="close-button">Cerrar</button>
+      <div class="drop-zone" @dragover.prevent @drop.prevent="handleDrop">
+        <div v-if="filePreview || fileName" class="file-preview">
+          <img v-if="isImage" :src="filePreview" alt="Vista previa del archivo" />
+          <p v-else class="file-name-full">{{ fileName }}</p>
+        </div>
+        <span v-else>Arrastra y suelta tu archivo aquí</span>
+      </div>
+      <button @click="uploadFiles" class="accept-button">Subir Archivo</button>
     </div>
+    <!--
+    <div v-if="successMessage" class="success-message">
+      <i class="fas fa-check-circle"></i> {{ successMessage }}
+    </div>
+    <div v-if="errorMessage" class="error-message">
+      <i class="fas fa-exclamation-circle"></i> {{ errorMessage }}
+    </div>
+    -->
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   props: {
     isVisible: {
       type: Boolean,
       required: true,
     },
+    handleSuccessMessage: {
+      type: Function,
+      required: true,
+    },
+    handleErrorMessage: {
+      type: Function,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      file: null,
+      fileName: null,
+      filePreview: null,
+      fileType: null,
+      isDragging: false,
+      dragStartX: 0,
+      dragStartY: 0,
+      modalStyle: {
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        position: 'absolute',
+      },
+      
+    };
   },
   methods: {
     closeModal() {
       this.$emit('close');
     },
     handleFileUpload(event) {
-      const files = event.target.files;
-      this.uploadFiles(files);
+      const selectedFile = event.target.files[0];
+      if (this.validateFile(selectedFile)) {
+        this.file = selectedFile;
+        this.fileName = selectedFile.name;
+        this.previewFile(selectedFile);
+      } else {
+        event.target.value = null; // Limpiar la selección si el archivo no es válido
+        this.fileName = null;
+        this.filePreview = null;
+        this.fileType = null;
+      }
     },
     handleDrop(event) {
-      const files = event.dataTransfer.files;
-      this.uploadFiles(files);
+      event.preventDefault(); // Prevenir que el navegador abra el archivo
+      const droppedFile = event.dataTransfer.files[0];
+      if (this.validateFile(droppedFile)) {
+        this.file = droppedFile;
+        this.fileName = droppedFile.name;
+        this.previewFile(droppedFile);
+      } else {
+        this.fileName = null;
+        this.filePreview = null;
+        this.fileType = null;
+      }
     },
-    uploadFiles(files) {
-      console.log("Archivos subidos:", files);
-      // Aquí puedes añadir la lógica para manejar los archivos subidos
+    validateFile(file) {
+      if (file) {
+        if (file.size > 10000000) { // Ejemplo de validación: tamaño máximo 10MB
+          alert("El archivo es demasiado grande.");
+          return false;
+        }
+        // Otras validaciones pueden ir aquí (tipo de archivo, etc.)
+        return true;
+      }
+      return false;
+    },
+    async uploadFiles() {
+      if (!this.file) {
+        alert("Por favor selecciona un archivo.");
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append("file", this.file);
+
+      try {
+        const response = await axios.post('/pedidos/upload/diario', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+        this.handleSuccessMessage(response.data.Mensaje || "Archivo subido exitosamente."); // Llama al método de éxito
+        this.errorMessage = null; // Limpiar mensaje de error si hay uno previo
+        setTimeout(() => {
+          this.successMessage = null; // Ocultar el mensaje después de unos segundos
+        }, 3000);
+        // Limpiar el mensaje de error si hay uno previo
+        this.handleErrorMessage(null);
+        // Cerrar el modal después de subir el archivo
+        this.closeModal();
+      } catch (error) {
+        console.error(error);
+        this.handleErrorMessage(error.response.data.Error || "Error al subir el archivo."); // Llama al método de error
+        // Limpiar el mensaje de éxito si hay uno previo
+        this.handleSuccessMessage(null);
+      }
+      this.closeModal();  // Cerrar el modal después de subir el archivo
+    },
+    previewFile(file) {
+      if (file.type.startsWith('image')) {
+        this.fileType = 'image';
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          this.filePreview = e.target.result;
+        };
+        reader.readAsDataURL(file);
+      } else {
+        this.fileType = 'other';
+        this.filePreview = null; // No mostrar vista previa para archivos que no son imágenes
+      }
+    },
+    startDrag(event) {
+      const modal = this.$refs.modal;
+      this.isDragging = true;
+      this.dragStartX = event.clientX - modal.getBoundingClientRect().left;
+      this.dragStartY = event.clientY - modal.getBoundingClientRect().top;
+      document.addEventListener('mousemove', this.onDrag);
+      document.addEventListener('mouseup', this.stopDrag);
+    },
+    onDrag(event) {
+      if (this.isDragging) {
+        this.modalStyle.left = `${event.clientX - this.dragStartX}px`;
+        this.modalStyle.top = `${event.clientY - this.dragStartY}px`;
+        this.modalStyle.transform = 'none';
+      }
+    },
+    stopDrag() {
+      this.isDragging = false;
+      document.removeEventListener('mousemove', this.onDrag);
+      document.removeEventListener('mouseup', this.stopDrag);
+    },
+  },
+  computed: {
+    isImage() {
+      return this.fileType === 'image';
     },
   },
 };
@@ -58,40 +203,148 @@ export default {
   padding: 20px;
   border-radius: 8px;
   text-align: center;
-  width: 400px;
+  width: 600px; /* Aumenta el tamaño del modal */
+  height: 400px; /* Aumenta el tamaño del modal */
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  position: absolute; /* Cambia a posición absoluta */
+  cursor: move; /* Cambia el cursor a mover */
 }
 
-.popup h3 {
+.popup h1 {
   color: #ffffff;
   margin-bottom: 20px;
+  user-select: none; /* Evita la selección de texto mientras se arrastra */
+}
+
+.file-input-container {
+  margin: 20px 0;
+}
+
+input[type="file"] {
+  display: none;
+}
+
+.file-input-label {
+  background: #ff6589;
+  color: #ffffff;
+  padding: 10px 20px;
+  border-radius: 8px;
+  cursor: pointer;
+  display: inline-block;
+}
+
+.file-input-label:hover {
+  background: #e05475;
+}
+
+.file-name-full {
+  color: #ffffff;
+  margin-top: 10px;
+  word-wrap: break-word;
+  text-align: center;
 }
 
 .drop-zone {
   border: 2px dashed #ccc;
-  padding: 20px;
-  margin-top: 10px;
+  padding: 80px; /* Aumenta el tamaño del área de drop */
+  margin-top: 20px;
   border-radius: 8px;
   color: #aaa;
+  height: 150px; /* Aumenta el tamaño del área de drop */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
 }
 
-input[type="file"] {
-  display: block;
-  margin: 20px auto;
-  color: #ffffff;
+.file-preview {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
+.file-preview img {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+}
+
+.accept-button,
 .close-button {
-  background: #ff6589;
+  background: #2dbd40;
   border: none;
   border-radius: 8px;
   padding: 10px 20px;
   cursor: pointer;
   color: white;
   margin-top: 20px;
+  position: relative; /* Asegura que los botones se mantengan en su lugar */
+}
+
+.accept-button:hover {
+  background: #299e39;
+}
+/*
+.accept-button:hover,
+.close-button:hover {
+  background: #0ad61e;
+}*/
+
+.close-button {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: transparent;
+  color: #ffffff;
+  font-size: 60px; /* Aumenta el tamaño de la fuente */
+  line-height: 32px;
+  padding: 0;
+  border: none;
+  cursor: pointer;
 }
 
 .close-button:hover {
-  background: #e05475;
+  color: #ebdbdb;
 }
+
+.success-message {
+  position: fixed;
+  top: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #28a745;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+.success-message i {
+  margin-right: 10px;
+}
+
+.error-message {
+  position: fixed;
+  top: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #dc3545; /* Color rojo para el mensaje de error */
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+.error-message i {
+  margin-right: 10px;
+}
+
 </style>
--- src/pages/Operario/listadoPaquetes.vue
+++ src/pages/Operario/listadoPaquetes.vue
@@ -4,38 +4,16 @@
     <div class="search-and-actions">
       <input type="text" placeholder="Buscar" v-model="searchword" />
       <button @click="anadirPaquete">+ Registrar Envío</button>
-      <!--
-      <input type="file" @change="handleFileUpload"/>
-      <button @click="uploadFile">
-        <i class="fa fa-upload"></i>
-      </button>-->
       <!-- Botón para abrir el popup -->
-
       <button @click="openModal" class="upload-button">
         <i class="fa fa-upload"></i>
       </button>
-      <FileUploadModal v-if="isModalOpen" :isVisible="isModalOpen" @close="closeModal" />
-      <!--
-      <button @click="openPopup" class="upload-button">
-        <i class="fa fa-upload"></i>
-      </button>
-      Popup de subida de archivos
-      <div v-if="isPopupOpen" class="popup-overlay" @click="closePopup">
-        <div class="popup" @click.stop>
-          <h3>Subir Archivos</h3>
-          <input type="file" @change="handleFileUpload" ref="fileInput" />
-          <div class="drop-zone" @dragover.prevent @drop="handleDrop">
-            Arrastra y suelta tus archivos aquí
-          </div>
-          <button @click="closePopup">Cerrar</button>
-        </div> 
-      </div>-->
-      <!--<button @click="updateStatus">Actualizar Estados</button>-->
-      <!--<label for="fileInput">
-        <input type="file" id="fileInput" @change="handleFileUpload">
-        <i class="fa fa-upload"></i> Subir Archivo
-      </label>-->
+      <FileUploadModal v-if="isModalOpen" :isVisible="isModalOpen" @close="closeModal" 
+                  :handleSuccessMessage="handleSuccessMessage" :handleErrorMessage="handleErrorMessage" />
+      
     </div>
+    
+    
     <base-table-envios
       :columns="tableColumns"
       :data="filteredData"
@@ -48,6 +26,13 @@
       :total-pages="totalPages"
       @page-changed="handlePageChange"
     />
+    <!-- Ajustar la posición de los mensajes de éxito y error -->
+    <div v-if="successMessage" class="success-message right-message">
+      <i class="fas fa-check-circle"></i> {{ successMessage }}
+    </div>
+    <div v-if="errorMessage" class="error-message right-message">
+      <i class="fas fa-exclamation-circle"></i> {{ errorMessage }}
+    </div>
   </div>
 </template>
 
@@ -84,6 +69,8 @@ export default {
       currentPage: 1,
       totalPages: 10,
       isModalOpen: false,
+      successMessage: null,
+      errorMessage: null,
     };
   },
   computed: {
@@ -106,6 +93,20 @@ export default {
     handlePageChange(page) {
       this.currentPage = page;
       console.log('Página actual:', page);
+    },
+    // Método para manejar el mensaje de éxito
+    handleSuccessMessage(message) {
+      this.successMessage = message;
+      setTimeout(() => {
+        this.successMessage = null; // Ocultar el mensaje después de unos segundos
+      }, 3000);
+    },
+    // Método para manejar el mensaje de error
+    handleErrorMessage(message) {
+      this.errorMessage = message;
+      setTimeout(() => {
+        this.errorMessage = null; // Ocultar el mensaje después de unos segundos
+      }, 3000);
     },
     fetchDataListaEnvios() {
       axios.get(urlBase + urlListarEnvios)
@@ -201,6 +202,53 @@ export default {
 .upload-button img {
   width: 32px;
   height: 32px;
+}
+
+.success-message,
+.error-message {
+  position: fixed;
+  top: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: rgba(40, 167, 69, 0.9); /* Cambiar el color de fondo para el mensaje de éxito */
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+.error-message {
+  background-color: rgba(220, 53, 69, 0.9); /* Cambiar el color de fondo para el mensaje de error */
+}
+
+.success-message i,
+.error-message i {
+  margin-right: 10px;
+}
+
+/* Otros estilos... */
+/* Ajustar los estilos para los mensajes de éxito y error */
+.right-message {
+  position: fixed;
+  top: 10px;
+  right: 900px; /* Ajustar la posición a la derecha */
+  max-width: 300px; /* Limitar el ancho para evitar que los mensajes ocupen toda la pantalla */
+  background-color: rgba(40, 167, 69, 0.9); /* Cambiar el color de fondo para el mensaje de éxito */
+  color: white;
+  padding: 10px 80px;
+  border-radius: 5px;
+  display: flex;
+  align-items: right;
+  justify-content: space-between; /* Alinear el ícono y el texto */
+  font-size: 16px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+.error-message.right-message {
+  background-color: rgba(220, 53, 69, 0.9); /* Cambiar el color de fondo para el mensaje de error */
 }
 
 </style>
