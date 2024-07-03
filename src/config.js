09a37135620817745c4561a75ba579124a82eaf9
U20170976 <stefano.alvarado@pucp.edu.pe>
2024-07-03 13:38:05 -0500

    Pruebas 4.5

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export default {
     teal: "#00d6b4",
     primaryGradient: ['rgba(76, 211, 150, 0.1)', 'rgba(53, 183, 125, 0)', 'rgba(119,52,169,0)'],
   },
-  saltoTemporalSimulacionIniciar: 4,
+  saltoTemporalSimulacionIniciar: 2,
   NSimulacionIniciar: 10,
   FitnessSimulacionIniciar: 20000, 
   saltoTemporalSimulacionContinuar: 2,
@@ -16,7 +16,7 @@ export default {
   delaySimulacion: 500,
   //urlBase: 'http://localhost'
   //urlBase: 'http://localhost:8080' //con el puerto modificado de Renzo
-  urlBase: 'http://127.0.0.1:8080'  //con el puerto modificado de Renzo
-  //urlBase: 'https://inf226-982-3c.inf.pucp.edu.pe/tomcat'
+  //urlBase: 'http://127.0.0.1:8080'  //con el puerto modificado de Renzo
+  urlBase: 'https://inf226-982-3c.inf.pucp.edu.pe/tomcat'
   
 }
